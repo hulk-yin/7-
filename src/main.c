408721a4addaa2f3bf12e41894e109b664d85d56
Hulk <yinkehao@enn.cn>
2025-02-12 17:46:36 +0800

    chore: up

--- src/main.c
+++ src/main.c
@@ -1,1 +1,18 @@
-void app_main() {}
+#include "esp_wifi.h"
+
+void app_main()
+{
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+
+  wifi_config_t wifi_config = {
+      .sta = {
+          .ssid = "your_ssid",
+          .password = "your_password",
+      },
+  };
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+}
